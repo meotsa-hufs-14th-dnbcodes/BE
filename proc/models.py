8417a1b812fae64338774ab4f80ef35d8b8781c7
sun-ji054 <seonji04203645@gmail.com>
2026-08-17 00:14:28 +0900

    feat: proc/models.y 파일 수정 \n 시술 테이블에 지속기간 추가

--- proc/models.py
+++ proc/models.py
@@ -15,6 +15,10 @@ class Procedure(models.Model):
     )
     proc_code = models.CharField(max_length=20, primary_key=True, verbose_name="시술 코드")
     proc_name = models.CharField(max_length=50, verbose_name="시술명")
+    proc_duration = models.PositiveIntegerField(
+        help_text="지속 기간(개월 단위, 예: 1, 6, 12)",
+        verbose_name="지속기간(개월)"
+    )
 
     def __str__(self):
-        return f"[{self.proc_code}] {self.proc_name}"
+        return f"[{self.proc_code}] {self.proc_name} ({self.proc_duration}개월)"
