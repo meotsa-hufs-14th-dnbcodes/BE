063bdbf378b41f1ededbe6672dbf62a7a538c5ec
sun-ji054 <seonji04203645@gmail.com>
2026-08-17 21:17:58 +0900

    chore: settings.py에 accouts 추가

--- chronoProject/settings.py
+++ chronoProject/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'proc',
+    'accounts',
 ]
 
 MIDDLEWARE = [
