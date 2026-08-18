09a5b0bc902e0f3b935407dfb4e3772cf00963fc
sun-ji054 <seonji04203645@gmail.com>
2026-08-18 21:03:02 +0900

    feat: apps에 체크리스트 추가

--- checklist/urls.py
+++ checklist/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ChecklistAPIView, WeeklyChecklistAPIView
 
 urlpatterns = [
-    path('checklist', ChecklistAPIView.as_view(), name='daily-checklist'),
-    path('weeklyChecklist', WeeklyChecklistAPIView.as_view(), name='weekly-checklist'),
+    path('checklist/', ChecklistAPIView.as_view(), name='daily-checklist'),
+    path('weeklyChecklist/', WeeklyChecklistAPIView.as_view(), name='weekly-checklist'),
 ]
--- chronoProject/settings.py
+++ chronoProject/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'proc',
     'rest_framework.authtoken',
     'accounts',
+    'checklist',
 ]
 
 MIDDLEWARE = [
